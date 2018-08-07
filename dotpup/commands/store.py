e018cca1ee7d45d5c790443a40df016b21638f04
Tamas Szelei <szelei.t@gmail.com>
2018-08-07 17:46:57 +0200

    store: refactor saving the operation into a separate function

--- dotpup/commands/store.py
+++ dotpup/commands/store.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from os import makedirs
 
 from dotpup.utils import symlink, get_repo_path
 
@@ -15,35 +14,20 @@ class StoreCommand(BaseCommand):
         desc="Moves a file to the repository, creates a symlink in place "
         "of the original and records the operation")
     self.parser.add_argument("target_file")
-    #self.parser.add_argument("repo_file")
 
   def run(self, args):
     import shutil
-    import platform
-    import dotpup.config as config
-    from dotpup.utils import unexpand, remove_prefix, get_repo_path
+    from dotpup.utils import remove_prefix, get_repo_path, save_operation
 
     # Move to repo and symlink back
     repo = get_repo_path()
-    target_file = str(Path(args.target_file).resolve())
+    target_file = Path(args.target_file).absolute()
     repo_file = Path(f"{repo}/{remove_prefix(target_file)}")
-    print(f"Moving {target_file} to {repo_file}")
-    makedirs(repo_file.parent, exist_ok=True)
+    repo_file.parent.mkdir(exist_ok=True, parents=True)
     shutil.move(args.target_file, repo_file)
-    print(f"Linking {repo_file} to {target_file}")
     symlink(repo_file, target_file)
 
-    cfg = config.load_config()
-    if "operations" not in cfg:
-      cfg["operations"] = {}
-
-    dst = remove_prefix(target_file)
-    os_name = platform.system()
-    if os_name not in cfg["operations"]:
-      cfg["operations"][os_name] = {}
-    cfg["operations"][os_name][dst] = unexpand(target_file)
-    config.save_config(cfg)
-
+    save_operation(repo_file, target_file)
     return 0
 
 
--- dotpup/utils.py
+++ dotpup/utils.py
@@ -17,22 +17,42 @@ def shellify_var(var_name: str) -> str:
     return f"${var_name}"
 
 
-def unexpand(filepath: str, env_var="HOME") -> str:
+def unexpand(filepath, env_var="HOME") -> str:
+  filepath = str(filepath)
   val = os.getenv(env_var)
   return filepath.replace(val, shellify_var(env_var))
 
 
-def remove_prefix(filepath: str, env_var="HOME") -> str:
+def remove_prefix(filepath, env_var="HOME") -> str:
+  filepath = str(filepath)
   val = os.getenv(env_var)
   unprefixed = filepath.replace(val, "")
+  print(unprefixed)
   if unprefixed[0] in ("/", "\\"):
     unprefixed = unprefixed[1:]
   return unprefixed
 
 
 def symlink(src: Path, dst: Path, record=True):
+  src = src.absolute()
+  dst = dst.absolute()
   system = platform.system()
   if system != "Windows":
     os.symlink(src, dst)
   else:
     raise NotImplementedError()
+
+
+def save_operation(repo_file: Path, target_file: Path):
+  repo_file = str(repo_file.absolute().relative_to(get_repo_path()))
+  target_file = str(unexpand(target_file.absolute()))
+
+  cfg = config.load_config()
+  if "operations" not in cfg:
+    cfg["operations"] = {}
+
+  os_name = platform.system()
+  if os_name not in cfg["operations"]:
+    cfg["operations"][os_name] = {}
+  cfg["operations"][os_name][repo_file] = target_file
+  config.save_config(cfg)
