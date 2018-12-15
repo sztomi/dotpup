7525a5cea730697e89c195819b3e41119eb4376b
Tamás Szelei <szelei.t@gmail.com>
2018-12-15 14:52:41 +0100

    Add project URL

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from os import getenv
 
 setup(
     name="dotpup",
-    version=getenv("DOTPUP_VERSION", "1.0.0"),
+    version=getenv("DOTPUP_VERSION", "1.0.1"),
     packages=find_packages(),
     license="MIT",
+    url="https://github.com/sztomi/dotpup",
     long_description=open("README.md").read(),
     entry_points={
         "console_scripts": ["dpup=dotpup.cli:cli"],
